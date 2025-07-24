49e54cefe475af80a82f094694ca9b6c75461735
Gopika M <gopikam272003@gmail.com>
2025-07-24 13:33:50 +0530

    filter created

--- CouponlyProject/src/app/features/admin/components/contact/contact.component.ts
+++ CouponlyProject/src/app/features/admin/components/contact/contact.component.ts
@@ -1,9 +1,18 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { CardBodyComponent, CardComponent, CardHeaderComponent, ColComponent, TableActiveDirective, TableDirective } from '@coreui/angular';
+import { ButtonCloseDirective, ButtonDirective, CardBodyComponent, CardComponent, CardHeaderComponent, ColComponent, ModalBodyComponent, ModalComponent, ModalFooterComponent, ModalHeaderComponent, ModalTitleDirective, ModalToggleDirective, TableActiveDirective, TableDirective } from '@coreui/angular';
 
 @Component({
   selector: 'app-contact',
-  imports: [ColComponent,CardComponent,CardHeaderComponent,CardBodyComponent,TableDirective],
+  imports: [ColComponent,CardComponent,CardHeaderComponent,CardBodyComponent,TableDirective,
+    ButtonDirective,
+    ModalToggleDirective,
+    ModalComponent,
+    ModalHeaderComponent,
+    ModalTitleDirective,
+    ButtonCloseDirective,
+    ModalBodyComponent,
+    ModalFooterComponent,CommonModule],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.scss'
 })
