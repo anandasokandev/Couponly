b02bf108231da9bb61317c027e170152534323c8
anandasokandev <ianandasokan@gmail.com>
2025-07-25 09:42:43 +0530

    location modal fixed 1.1

--- CouponlyProject/src/app/features/admin/pages/add-location-modal/add-location-modal.component.ts
+++ CouponlyProject/src/app/features/admin/pages/add-location-modal/add-location-modal.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-import { ButtonCloseDirective, ButtonDirective, FormControlDirective, FormDirective, FormLabelDirective, ModalBodyComponent, ModalComponent, ModalFooterComponent, ModalHeaderComponent, ModalTitleDirective, ModalToggleDirective } from '@coreui/angular';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { ButtonCloseDirective, ButtonDirective, FormControlDirective, FormDirective, FormLabelDirective, FormModule, ModalBodyComponent, ModalComponent, ModalFooterComponent, ModalHeaderComponent, ModalTitleDirective, ModalToggleDirective, ToasterComponent, ToasterPlacement } from '@coreui/angular';
 
 @Component({
   selector: 'app-add-location-modal',
@@ -13,11 +14,30 @@ import { ButtonCloseDirective, ButtonDirective, FormControlDirective, FormDirect
     FormDirective,
     FormLabelDirective,
     FormControlDirective,
-    ButtonDirective
+    ButtonDirective,
+    ReactiveFormsModule
   ],
   templateUrl: './add-location-modal.component.html',
   styleUrl: './add-location-modal.component.scss'
 })
 export class AddLocationModalComponent {
+
+  locationForm: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    this.locationForm = this.fb.group({
+      district: ['', Validators.minLength(2)],
+      locationName: ['', Validators.minLength(2)],
+      pincode: [''],
+      latitude: [''],
+      longitude: ['']
+    });
+  }
+
+  save() {
+    if (this.locationForm.valid) {
+      console.log(this.locationForm.value);
+    }
+  }
 
 }
