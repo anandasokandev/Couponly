0f363958336d55f7737a26704d0d899c231c49e4
Anumol K <anumolammu2003@gmail.com>
2025-07-28 11:19:43 +0530

    custom toast

---
+++ CouponlyProject/src/app/features/admin/pages/custom-toast/custom-toast.component.ts
@@ -0,0 +1,21 @@
+import { Component, input } from '@angular/core';
+
+@Component({
+  selector: 'app-custom-toast',
+  imports: [],
+  templateUrl: './custom-toast.component.html',
+  styleUrl: './custom-toast.component.scss'
+})
+export class CustomToastComponent {
+//   @input() message = '';
+//   @Input() type: 'success' | 'error' | 'info' = 'info';
+//   show = false;
+
+//   showToast(message: string, type: 'success' | 'error' | 'info' = 'info') {
+//     this.message = message;
+//     this.type = type;
+//     this.show = true;
+
+//     setTimeout(() => this.show = false, 3000); // Auto close after 3 sec
+//   }
+}
