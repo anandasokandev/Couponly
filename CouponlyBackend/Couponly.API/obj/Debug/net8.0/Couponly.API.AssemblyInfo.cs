b4a341dd12cbdd145bb8a5218c74482722eb6af6
anandasokandev <ianandasokan@gmail.com>
2025-07-29 13:10:54 +0530

    staged changes

--- CouponlyBackend/Couponly.API/obj/Debug/net8.0/Couponly.API.AssemblyInfo.cs
+++ CouponlyBackend/Couponly.API/obj/Debug/net8.0/Couponly.API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Couponly.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0a6e32306d37c74d3776a5996671583c78c48cb8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+24b1d04159abc8028b480397d7e138f2edb2558d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Couponly.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Couponly.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
