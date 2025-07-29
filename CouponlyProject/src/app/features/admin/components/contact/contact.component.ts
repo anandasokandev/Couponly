7c1be711af21edf6510bf72b5144359a3aac8856
Gopika M <gopikam272003@gmail.com>
2025-07-29 13:26:38 +0530

    toast design implemented

--- CouponlyProject/src/app/features/admin/components/contact/contact.component.ts
+++ CouponlyProject/src/app/features/admin/components/contact/contact.component.ts
@@ -1,17 +1,21 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { ButtonCloseDirective, ButtonDirective, CardBodyComponent, CardComponent, CardHeaderComponent, ColComponent, ModalBodyComponent, ModalComponent, ModalFooterComponent, ModalHeaderComponent, ModalTitleDirective, ModalToggleDirective, TableActiveDirective, TableDirective } from '@coreui/angular';
+import {  CardBodyComponent, CardComponent, CardHeaderComponent, ColComponent, ModalBodyComponent, ModalComponent, ModalFooterComponent, ModalHeaderComponent, ModalTitleDirective, ModalToggleDirective, TableActiveDirective, TableDirective } from '@coreui/angular';
 import { EditContactModalComponent } from '../../pages/edit-contact-modal/edit-contact-modal.component';
 import { AddContactModalComponent } from '../../pages/add-contact-modal/add-contact-modal.component';
 
 @Component({
   selector: 'app-contact',
-  imports: [ColComponent,CardComponent
-    ,CardHeaderComponent,CardBodyComponent,TableDirective,
+  imports: [ColComponent,
+    CardComponent,
+    CardHeaderComponent,
+    CardBodyComponent,
+    TableDirective,
     ModalToggleDirective,
     ModalComponent,
     CommonModule,
-    EditContactModalComponent,AddContactModalComponent],
+    EditContactModalComponent,
+    AddContactModalComponent],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.scss'
 })
