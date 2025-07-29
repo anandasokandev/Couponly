94d5393bd983a2a7d58194c8de7e4c1b31ca20da
anandasokandev <ianandasokan@gmail.com>
2025-07-29 11:01:03 +0530

    store toast fixed

--- CouponlyProject/src/app/features/admin/pages/add-store-modal/add-store-modal.component.ts
+++ CouponlyProject/src/app/features/admin/pages/add-store-modal/add-store-modal.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { ButtonCloseDirective, ButtonDirective, FormControlDirective, FormDirective, FormLabelDirective, ModalBodyComponent, ModalComponent, ModalFooterComponent, ModalHeaderComponent, ModalTitleDirective, ModalToggleDirective } from '@coreui/angular';
-import { CustomToastService } from 'src/app/commons/services/custom-toast.service';
+import { CustomToastService } from '../../../../commons/services/custom-toast.service';
 
 @Component({
   selector: 'app-add-store-modal',
