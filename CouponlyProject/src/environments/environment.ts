75c4a8c294367525c6def8480b73ed4f14f0a379
anandasokandev <ianandasokan@gmail.com>
2025-09-02 15:00:44 +0530

    coupon generation

--- CouponlyProject/src/environments/environment.ts
+++ CouponlyProject/src/environments/environment.ts
@@ -1,6 +1,7 @@
 export const environment = {
   production: true,
-  apiBaseUrl: 'https://api.couponly.store/api',
+  apiBaseUrl: 'https://localhost:7224/api',
+  // apiBaseUrl: 'https://api.couponly.store/api',
   imageUploadUrl: 'https://images.couponly.store/api/upload',
   imageKey: '5dtg419dtny9y84nu67ry7',
   endpoints: {
@@ -56,6 +57,9 @@ export const environment = {
     login:{
       signup:'/Login',
       forgotPassword:'/login/forgot-password'
+    },
+    coupon: {
+      GetCouponType: 'Coupon/CouponTypes'
     }
   }
 };
