390995d80aaf286e35288c2700a91ed87d4992a2
anandasokandev <ianandasokan@gmail.com>
2025-11-10 11:34:04 +0530

    Image generation method , keyword building, image preview added

--- CouponlyProject/src/app/features/admin/pages/ai-image-modal/ai-image-modal.component.ts
+++ CouponlyProject/src/app/features/admin/pages/ai-image-modal/ai-image-modal.component.ts
@@ -1,7 +1,9 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, EventEmitter, inject, Input, OnInit, Output } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { ButtonCloseDirective, ButtonDirective, CardBodyComponent, CardHeaderComponent, CardModule, FormModule, GridModule, InputGroupComponent, ModalBodyComponent, ModalComponent, ModalDialogComponent, ModalFooterComponent, ModalHeaderComponent, ModalTitleDirective, ModalToggleDirective } from '@coreui/angular';
+import { CouponService } from '../../../../commons/services/Coupon/coupon.service';
+import { ToastService } from '../../../../commons/services/Toaster/toast.service';
 
 @Component({
   selector: 'app-ai-image-modal',
@@ -26,7 +28,92 @@ import { ButtonCloseDirective, ButtonDirective, CardBodyComponent, CardHeaderCom
   templateUrl: './ai-image-modal.component.html',
   styleUrl: './ai-image-modal.component.scss'
 })
-export class AiImageModalComponent {
+export class AiImageModalComponent implements OnInit {
 
+  @Input() coupon: any; 
+  @Output() couponUrl = new EventEmitter<string>();
+  
   public generationMethod: 'prompt' | 'default' = 'prompt';
+  selectedCoupon: any = null;
+  isGenerating: boolean = false;
+  generatedCouponUrl: string = '';
+
+  couponService = inject(CouponService);
+  toast = inject(ToastService);
+  
+  ngOnInit(): void {
+
+  }
+
+  generateImage(promptInput: string) {
+    this.isGenerating = true;
+
+    if (this.generationMethod === 'prompt') {
+      this.couponService.generateAiImage({prompt:promptInput ,storeId:this.coupon.storeId}).subscribe({
+        next: (response) => {
+          if(response.isSuccess == true && response.statusCode == 200){
+            this.generatedCouponUrl = response.data.url;
+            this.isGenerating = false;
+          }else{
+            this.toast.show({ type: 'error', message: 'Failed to generate image. Please try again.' });
+            this.isGenerating = false;
+          }
+        },
+        error: (error) => {
+          this.toast.show({ type: 'error', message: 'An error occurred while generating the image.' });
+          console.error('Error generating coupon:', error);
+          this.isGenerating = false;
+        }
+      });
+    }
+    else {
+      // console.log(this.buildKeywords(this.coupon));
+      this.couponService.generateAiImage({prompt:this.buildKeywords(this.coupon) ,storeId:this.coupon.storeId}).subscribe({
+        next: (response) => {
+          if(response.isSuccess == true && response.statusCode == 200){
+            this.generatedCouponUrl = response.data.url;
+            this.isGenerating = false;
+          }else{
+            this.toast.show({ type: 'error', message: 'Failed to generate image. Please try again.' });
+            this.isGenerating = false;
+          }
+        },
+        error: (error) => {
+          this.toast.show({ type: 'error', message: 'An error occurred while generating the image.' });
+          console.error('Error generating coupon:', error);
+          this.isGenerating = false;
+        }
+      });
+    }
+  }
+
+
+  submitGeneratedImage() {
+    this.couponUrl.emit(this.generatedCouponUrl);
+  }
+
+  private loadPreviouslyGeneratedImages(storeId: number) {
+    
+  }
+
+  private buildKeywords(couponObj: Record<string, any>): string {
+    const keywords: string[] = [];
+
+    for (const [key, value] of Object.entries(couponObj)) {
+      if (value === null || value === undefined) continue;
+
+      // Skip storeId field
+      if (key.toLowerCase() === "storeid") continue;
+
+      // For nested objects, flatten them
+      if (typeof value === "object" && !Array.isArray(value)) {
+        for (const [innerKey, innerValue] of Object.entries(value)) {
+          keywords.push(`${key}-${innerKey}-${innerValue}`.toLowerCase());
+        }
+      } else {
+        keywords.push(`${key}-${value}`.toLowerCase());
+      }
+    }
+    return keywords.join(", ");
+  }
 }
